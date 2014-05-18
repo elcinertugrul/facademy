e1f7e752e4bb9354b4ec47cfffaa63d081da52ab
elcinertugrul@gmail.com <elcinertugrul@gmail.com>
2014-05-17 21:03:25 -0400

    progress json files

--- BIS/BIS/Program.cs
+++ BIS/BIS/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TToffice;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace BIS
 {
@@ -9,18 +12,128 @@ namespace BIS
     {
         static void Main(string[] args)
         {
-            //http://a810-bisweb.nyc.gov/bisweb/FacadeStatusInformationServlet?allisn=0000024332&allbin=1016880&requestid=2
+            
+            //1. read excel & get the list of all BINs
+            List<string> bins = new List<string>();
 
-            string url = "http://a810-bisweb.nyc.gov/bisweb/FacadesByLocationServlet?requestid=1&allbin=1016880";
-            //string url = "http://a810-bisweb.nyc.gov/bisweb/FacadesByLocationServlet?requestid=1&allbin=1009012";
-            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
-            System.Net.WebResponse myresponse = request.GetResponse();
-            // Open data stream:
-            System.IO.Stream _WebStream = myresponse.GetResponseStream();
-            //string path = @"C:\Temp\streetview";
-            var result = new System.Net.WebClient().DownloadData(url);
+            TT_Excel xl = new TT_Excel();
+            xl.openInvisible(@"C:\Users\ELCIN\Documents\GitHub\facademy\ZipCode10010_BINs.xlsx");
+            string[] xldata;
+            xl.readColumn(1, 1, out xldata);
 
-            string astringHTML = new System.Net.WebClient().DownloadString(url);
+            xl.close();
+            xl.release();
+            xl = null;
+
+            bins = xldata.ToList();
+
+            //2. Loop over BINs & create list of object
+            List<facadedata> scrapeddata = new List<facadedata>();
+
+            foreach (var bin in bins)
+            {
+
+                facadedata bldgdata = new facadedata();
+
+
+                //http://a810-bisweb.nyc.gov/bisweb/FacadeStatusInformationServlet?allisn=0000024332&allbin=1016880&requestid=2
+                //string url = "http://a810-bisweb.nyc.gov/bisweb/FacadesByLocationServlet?requestid=1&allbin=1016880";
+                //string url = "http://a810-bisweb.nyc.gov/bisweb/FacadesByLocationServlet?requestid=1&allbin=1009012";
+
+                string url = "http://a810-bisweb.nyc.gov/bisweb/FacadesByLocationServlet?requestid=1&allbin=" + bin;
+
+                //System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+                //System.Net.WebResponse myresponse = request.GetResponse();
+                // Open data stream:
+                //System.IO.Stream _WebStream = myresponse.GetResponseStream();
+                
+                //string path = @"C:\Temp\streetview";
+                //var result = new System.Net.WebClient().DownloadData(url);
+                string astringHTML = string.Empty;
+                try
+                {
+                   astringHTML = new System.Net.WebClient().DownloadString(url);
+                }
+                catch (Exception)
+                {
+                    
+                   // throw; // do nothing
+                }
+
+
+                if (astringHTML != string.Empty)
+                {
+                    string[] splitone = { "ErrorMsg :: " };
+                    string[] words = astringHTML.Split(splitone, StringSplitOptions.None);
+
+                    if (words[1].StartsWith("\n")) // there is data available keep contunie
+                    {
+                        string[] splittwo = { "\nVlNumHous :: ", "\nNmStrt :: " };
+                        string[] wordstwo = words[1].Split(splittwo, StringSplitOptions.None);
+
+                        bldgdata.NumHous = wordstwo[1];
+
+                        string[] splitthree = { "\nVlNmStrt :: ", "\nNmBoro :: " };
+                        string[] wordsthree = wordstwo[2].Split(splitthree, StringSplitOptions.None);
+
+                        bldgdata.StrtName = wordsthree[0];
+
+                        string[] splitfour = { "\nVlBin :: " };
+                        string[] wordsfour = wordsthree[1].Split(splitfour, StringSplitOptions.None);
+
+                        bldgdata.Boro = wordsfour[0];
+
+                        string[] splitfive = { "\nVlNumZip :: " };
+                        string[] wordsfive = wordsfour[1].Split(splitfive, StringSplitOptions.None);
+
+                        bldgdata.BIN = wordsfive[0];
+
+                        string[] splitsix = { "\nVlTaxBlock :: " };
+                        string[] wordssix = wordsfive[1].Split(splitsix, StringSplitOptions.None);
+
+                        bldgdata.Zip = wordssix[0];
+
+                        string[] splitseven = { "[0:FCycle]{" };
+                        string[] wordseven = wordssix[1].Split(splitseven, StringSplitOptions.None);
+
+                        string[] spliteight = { "}" };
+                        int a = wordseven.ToList().Count - 1;
+                        string[] wordnine = wordseven[a].Split(spliteight, StringSplitOptions.None);
+
+                        bldgdata.Cycle = wordnine[0];
+
+                        string[] splitnine = { "[5:FCurrentStatus]{" };
+                        string[] wordsnine = wordssix[1].Split(splitnine, StringSplitOptions.None);
+
+                        int b = wordsnine.ToList().Count - 1;
+                        string[] wordeleven = wordsnine[b].Split(spliteight, StringSplitOptions.None);
+
+                        bldgdata.CurrentStatus = wordeleven[0];
+
+                        string[] splitten = { "[7:FNumStories]{" };
+                        string[] wordtwelve = wordssix[1].Split(splitten, StringSplitOptions.None);
+
+                        int c = wordtwelve.ToList().Count - 1;
+                        string[] wordthirteen = wordtwelve[c].Split(spliteight, StringSplitOptions.None);
+
+                        bldgdata.NumStories = wordthirteen[0];
+
+                        scrapeddata.Add(bldgdata);
+
+                        //request.Abort();
+                        //myresponse.Close();
+
+                    } 
+                }
+            }
+            //3. exchange List of facadedata to json
+
+            
+            var Json = new JavaScriptSerializer().Serialize(scrapeddata.ToArray());
+
+            File.WriteAllText(@"C:\Users\ELCIN\Desktop\JSONSrapperdata10010.txt", Json);
+            File.WriteAllText(@"C:\Users\ELCIN\Desktop\JSONSrapperdata10010.json", Json);
+
             
         }
     }
---
+++ BIS/BIS/facadedata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BIS
+{
+    public class facadedata
+    {
+        public string BIN;
+        public string NumHous;
+        public string StrtName;
+        public string Boro;
+        public string Zip;
+        public string NumStories;
+        public string Cycle;
+        public string CurrentStatus;
+
+        public facadedata() { }
+    }
+}
