50bbda74e093800663daa72a4cc3b8651c7064b7
eertugrul@thorntontomasetti.com <eertugrul@thorntontomasetti.com>
2014-07-04 12:29:15 -0400

    bis cycles per bin

--- BIS/BIS/Program.cs
+++ BIS/BIS/Program.cs
@@ -75,24 +75,13 @@ namespace BIS
                         for (int i = 1; i < wordscycles.ToList().Count; i++)
                         {
                             cycle acycle = new cycle();
-                            string[] splitthree = { "}\n","[1:FControlNumber]{", "[5:FCurrentStatus]{", "[8:FInitFileDate]{", "[9:FaIsn]{" };
+                            //string[] splitthree = { "}\n","[1:FControlNumber]{", "[5:FCurrentStatus]{", "[8:FInitFileDate]{", "[9:FaIsn]{" };
                             string[] splitfour = { "}\n", "[1:FControlNumber]{", "[2:FHouseNumber]{", "[3:FStreetName]{", "[5:FCurrentStatus]{", "[6:FBin]{", "[7:FNumStories]{","[8:FInitFileDate]{", "[9:FaIsn]{" };
 
+                            string[] wordsinfo = wordscycles[i].Split(splitfour, StringSplitOptions.None);
 
-                            if (i != 1)
+                            if (wordsinfo[11] == bin)
                             {
-                                string[] wordsinfo = wordscycles[i].Split(splitthree, StringSplitOptions.None);
-
-                                acycle.FCycle = wordsinfo[0];
-                                acycle.FCtrlNum = wordsinfo[2];
-                                acycle.FCurStat = wordsinfo[7];
-                                acycle.FInitDate = wordsinfo[11];
-                                acycle.FaISN = wordsinfo[13];
-                            }
-                            else 
-                            {
-                                string[] wordsinfo = wordscycles[i].Split(splitfour, StringSplitOptions.None);
-
                                 acycle.FCycle = wordsinfo[0];
                                 acycle.FCtrlNum = wordsinfo[2];
                                 bldgdata.Num = wordsinfo[4];
@@ -102,7 +91,6 @@ namespace BIS
                                 bldgdata.NumStory = wordsinfo[13];
                                 acycle.FInitDate = wordsinfo[15];
                                 acycle.FaISN = wordsinfo[17];
-                            
                             }
 
                             bldgdata.Cycles.Add(acycle);
@@ -115,64 +103,14 @@ namespace BIS
                         bldgdata.Boro = wordsthree[1];
                         bldgdata.Zip = wordsthree[3];
 
-                        //string[] splittwo = { "\nVlNumHous :: ", "\nNmStrt :: " };
-                        //string[] wordstwo = words[1].Split(splittwo, StringSplitOptions.None);
-
-                        //bldgdata.NumHous = wordstwo[1];
-
-                        //string[] splitthree = { "\nVlNmStrt :: ", "\nNmBoro :: " };
-                        //string[] wordsthree = wordstwo[2].Split(splitthree, StringSplitOptions.None);
-
-                        //bldgdata.StrtName = wordsthree[0];
-
-                        //string[] splitfour = { "\nVlBin :: " };
-                        //string[] wordsfour = wordsthree[1].Split(splitfour, StringSplitOptions.None);
-
-                        //bldgdata.Boro = wordsfour[0];
-
-                        //string[] splitfive = { "\nVlNumZip :: " };
-                        //string[] wordsfive = wordsfour[1].Split(splitfive, StringSplitOptions.None);
-
-                        //bldgdata.BIN = wordsfive[0];
-
-                        //string[] splitsix = { "\nVlTaxBlock :: " };
-                        //string[] wordssix = wordsfive[1].Split(splitsix, StringSplitOptions.None);
-
-                        //bldgdata.Zip = wordssix[0];
-
-                        //string[] splitseven = { "[0:FCycle]{" };
-                        //string[] wordseven = wordssix[1].Split(splitseven, StringSplitOptions.None);
-
-                        //string[] spliteight = { "}" };
-                        //int a = wordseven.ToList().Count - 1;
-                        //string[] wordnine = wordseven[a].Split(spliteight, StringSplitOptions.None);
-
-                        //bldgdata.Cycle = wordnine[0];
-
-                        //string[] splitnine = { "[5:FCurrentStatus]{" };
-                        //string[] wordsnine = wordssix[1].Split(splitnine, StringSplitOptions.None);
-
-                        //int b = wordsnine.ToList().Count - 1;
-                        //string[] wordeleven = wordsnine[b].Split(spliteight, StringSplitOptions.None);
-
-                        //bldgdata.CurrentStatus = wordeleven[0];
-
-                        //string[] splitten = { "[7:FNumStories]{" };
-                        //string[] wordtwelve = wordssix[1].Split(splitten, StringSplitOptions.None);
-
-                        //int c = wordtwelve.ToList().Count - 1;
-                        //string[] wordthirteen = wordtwelve[c].Split(spliteight, StringSplitOptions.None);
-
-                        //bldgdata.NumStories = wordthirteen[0];
-
                         scrapeddata.Add(bldgdata);
-
-                        //request.Abort();
-                        //myresponse.Close();
-
                     } 
                 }
             }
+
+            //request.Abort();
+            //myresponse.Close();
+
             //3. exchange List of facadedata to json
             //"C:\Users\EErtugrul\Documents\GitHub\facademy\JSONSrapperdata10010.txt"
             
@@ -181,8 +119,8 @@ namespace BIS
             //File.WriteAllText(@"C:\Users\EErtugrul\Documents\GitHub\facademy\JSONSrapperdata10280.txt", Json);
             //File.WriteAllText(@"C:\Users\EErtugrul\Documents\GitHub\facademy\JSONSrapperdata10280.json", Json);
 
-            File.WriteAllText(@"C:\Users\ELCIN\Documents\GitHub\facademy\JSONSrapperdata10010TEST.txt", Json);
-            File.WriteAllText(@"C:\Users\ELCIN\Documents\GitHub\facademy\JSONSrapperdata10010TEST.json", Json);
+            File.WriteAllText(@"C:\Users\ELCIN\Documents\GitHub\facademy\JSONSrapperdata10010TEST_R.txt", Json);
+            File.WriteAllText(@"C:\Users\ELCIN\Documents\GitHub\facademy\JSONSrapperdata10010TEST_R.json", Json);
    
         }
     }
